d3da1f219ce7d3991b74a369ca6c46ee9236150c
Cameron <cameronbodie@gmail.com>
2020-08-12 16:15:05 -0500

    Add game widget

--- src/game.py
+++ src/game.py
@@ -6,21 +6,34 @@ from PyQt5.QtWidgets import *
 
 
 class GameHome(QtWidgets.QWidget):
-    def __init__(self, window, **kwargs):
-        super(GameHome, self).__init__(*args, **kwargs)
+    def __init__(self, parent=None):
+        super(GameHome, self).__init__(parent)
+        self.initUI()
+    def initUI(self):
 
-        #create a layout for all out widgets. QVBox means widgets will be aligned vertically.
-        layout = QVBoxLayout()
+            self.createGridLayout()
 
-        self.row_1 = QHBoxLayout()
+            windowLayout = QVBoxLayout()
+            windowLayout.addWidget(self.horizontalGroupBox)
+            self.setLayout(windowLayout)
 
-        self.btn_1_1 = QLabel("")
-        self.btn_1_1.setStyleSheet("background-color: grey")
-        self.btn_1_1.setFixedWidth(500)
-        self.row_1.addWidget(self.btn_1_1)
+            self.show()
 
-        layout.addChildLayout(self.row_1)
-        window.setLayout()
+    def createGridLayout(self):
+            self.horizontalGroupBox = QGroupBox("Grid")
+            layout = QGridLayout()
+
+            for x in range(3):
+                for y in range(3):
+                    btn = QPushButton("")
+                    btn.setFixedWidth(100)
+                    btn.setFixedHeight(100)
+                    layout.addWidget(btn, x, y)
+
+
+            self.horizontalGroupBox.setLayout(layout)
+    def game_btn_press(self, x, y):
+        print(f"btn: {x}, {y} pressed")
 
 
 
--- src/home.py
+++ src/home.py
@@ -6,16 +6,15 @@ from PyQt5.QtGui import QFont
 
 from PyQt5.QtWidgets import *
 
-class Home(QtWidgets.QWidget):
-    # Init our Home screen widget
+class HomeWidget (QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
-        super(Home, self).__init__(*args, **kwargs)
+        super(HomeWidget, self).__init__(*args, **kwargs)
 
         #create a layout for all out widgets. QVBox means widgets will be aligned vertically.
-        layout = QVBoxLayout()
+        self.layout = QVBoxLayout()
 
         # Set padding to layout:
-        layout.setContentsMargins(30, 30, 30, 30)
+        self.layout.setContentsMargins(30, 30, 30, 30)
 
         # To add anything to the screen we need to create it
         # and add it to the layout
@@ -23,10 +22,10 @@ class Home(QtWidgets.QWidget):
         label = QLabel("Welcome to TimelyClicks")
         label.setFont(QFont("Arial", 16, QFont.Bold))
         label.setAlignment(QtCore.Qt.AlignCenter)
-        layout.addWidget(label)
+        self.layout.addWidget(label)
 
         desc_label = QLabel("This game is all about reaction speed!\nOn the next page there is a 4x4 square of buttons. They will randomly show up an appear!\n Your job is to hit them before they go grey.")
-        layout.addWidget(desc_label)
+        self.layout.addWidget(desc_label)
 
         btn_group = QVBoxLayout()
         btn_group.setAlignment(QtCore.Qt.AlignCenter)
@@ -34,15 +33,27 @@ class Home(QtWidgets.QWidget):
         self.start_game_btn = QPushButton("Start Game")
         self.start_game_btn.setFixedWidth(100)
         self.start_game_btn.setStyleSheet(blue_btn())
-        self.start_game_btn.clicked.connect(self.button_clicked)
 
-        layout.addWidget(self.start_game_btn)
+        self.layout.addWidget(self.start_game_btn)
+        self.setLayout(self.layout)
+        self.show()
 
         #set the home layout
-        self.setLayout(layout)
 
-    def button_clicked(self):
-        print("start game btn pressed")
+class Home(QtWidgets.QMainWindow):
+    # Init our Home screen widget
+    def __init__(self, *args, **kwargs):
+        super(Home, self).__init__(*args, **kwargs)
+        self.homeWidget = HomeWidget()
+        self.homeWidget.start_game_btn.clicked.connect(self.start_game)
+        self.setCentralWidget(self.homeWidget)
+        self.show()
 
+    def start_game(self):
+        self.game_widget = GameHome()
+        self.setCentralWidget(self.game_widget)
+        self.show()
 
+if __name__ == "__main__":
+    home= Home()
 
